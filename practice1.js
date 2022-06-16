87013c3e891b2730794e4e6f5b4b9eaa6b7b5771
sotskovds <71659893+sotskovds@users.noreply.github.com>
2022-06-16 10:37:12 +0300

    master

--- practice1.js
+++ practice1.js
@@ -9,38 +9,42 @@ dog = switcher;
 console.log(cat);
 console.log(dog);
 
-
-
-
 // Ex.2 - Values and variables
 // 1. Огласите переменные 'company', 'position'  и запишите туда значения соответственно компании, в которой вы работаете.
 // 2. Залогируйте их вместе, используя функцию console.log() один раз
+
+const company ='WeSoftYou'; // 'let' to 'const' is changed due to Ex.4 requirements
+let position = 'QA'
+
+console.log(company, position)
 
 // Ex.3 - Data Types
 // 1. Объявите переменную с названием 'isProductCompany' и запишите значение в соответствии с компанией в которой работаете. Значение должно быть типа boolean. Также объявите переменную
 // 'projectName', но пока не записывайте туда никакого значения.
 // 2. Залогируйте тип переменных 'isProductCompany', 'company' и 'position' в консоль.
 
+let isProductCompany = false;
+let projectName;
+console.log(typeof isProductCompany);
+console.log(typeof company);
+console.log(typeof position);
 
 // Ex.4 - let, const and var
 // 1. Запишите значение в 'projectName' в соответствии с названием проекта над которым работаете
 // 2. Подумайте, какие из созданных ранее переменных следует объявить через "const" (какие значения могут измениться, а какие нет?). Затем измените эти переменные, используя "const".
 
-
+projectName = 'CreatoRanch'
 
 // Ex5. - найди ошибку (английский)
 // This code should store "jsfor.qa" as a variable called name but it's not working. Can you figure out why?
 
-// const a == "jsfo";
-// const b == "r.qa";
-// const name == a + b;
+const a = "jsfo"; // changed '==' to '=' as the equality operator (==) checks whether its two operands are equal, returning a Boolean result.
+const b = "r.qa"; 
+const name = a + b;
+
+console.log(name);
 
 
-// one more time, expected result when console.log(name) should be "jsfor.qa"
-
-
-// ЗАДАЧИ СО ЗВЕЗДОЧКОЙ =) 
-// Для тех кто хочет попрактиковаться в знаниях, которые, возможно, имеет и решить простые задачи из тем, которые будут далее и познакомиться с нативными объектами и функциями (мы их немного рассмотрели в типах уже):
 // Ex6. - написать функцию, которая будет суммировать строки математичесски. Пример:
 // "4", "5" --> "9"
 //  "34", "5" --> "39" 
@@ -48,12 +52,13 @@ console.log(dog);
 // "2", "" --> "2"
 //  "-5", "3" --> "-2"
 
+function sumStr(a, b) {
+    let num1 = Number(a);
+    let num2 = Number(b);
+    console.log(num1 + num2);
+}
 
-
-// function sumStr(a,b) {
-  
-// }
-
+sumStr('-2', '17.5');
 
 // Ex7. - написать функцию, которая принимает два аргумента и возвращает (логает) остачу после деления БОЛЬШЕГО аргумента на МЕНЬШЕЕ (не просто первое на второе)
 // remainder(17, 5) -> 2
@@ -61,11 +66,15 @@ console.log(dog);
 // remainder(1, 0) -> NaN (Division by 0)
 // remainder(0, 0) -> NaN (Division by 0)
 
-// function remainder(a, b){
-  // Divide the larger argument by the smaller argument and return the remainder
-  
-// }
-
+function remainder(a, b){
+    if ( a > b ) {
+        console.log(a % b);
+    } 
+    else {
+        console.log(b % a);
+    }
+}
+remainder (16, -72.8)
 
 // Ex8. - Get opposite number
 // Very simple, given a number, find its opposite.
@@ -74,10 +83,10 @@ console.log(dog);
 // 14: -14 
 // -34: 34  
 
-// function opposite(number) {
-  //your code
-// }
-
+function opposite(a){
+    console.log(-a)
+}
+opposite (-16)
 
 // Ex9. - Number to string
 // We need a function that can transform a number into a string.
@@ -86,7 +95,10 @@ console.log(dog);
 //  999 --> "999"
 
 
-
-// function numberToString(num) {
-  // Return a string of the number here!
-// }
+function numberToString(num) {
+    let str1 = String(num);
+    let str2 = num.toString();
+    console.log(str1, typeof str1);
+    console.log(str2, typeof str2);
+}
+numberToString(999);
