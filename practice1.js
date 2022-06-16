e612d7ec7d0b1254079d5ecfed4668fa3a81fa72
sotskovds <71659893+sotskovds@users.noreply.github.com>
2022-06-16 09:32:03 +0300

    first commit

---
+++ practice1.js
@@ -0,0 +1,93 @@
+// Ex. 1 - Поменять местами значения "cat" та "dog" 
+// let cat = 'cat';
+// let dog = 'dog';
+
+// Ваш код:
+// note: для решения этой задачи вам нужно огласить дополнительную переменную
+// то есть вариант
+//    cat = 'dog';
+//  dog = 'cat'; 
+// не подходит
+
+// Результат должен быть следующим:
+// console.log(cat); //  'dog'
+// console.log(dog); // 'cat'
+
+// Ex.2 - Values and variables
+// 1. Огласите переменные 'company', 'position'  и запишите туда значения соответственно компании, в которой вы работаете.
+// 2. Залогируйте их вместе, используя функцию console.log() один раз
+
+// Ex.3 - Data Types
+// 1. Объявите переменную с названием 'isProductCompany' и запишите значение в соответствии с компанией в которой работаете. Значение должно быть типа boolean. Также объявите переменную
+// 'projectName', но пока не записывайте туда никакого значения.
+// 2. Залогируйте тип переменных 'isProductCompany', 'company' и 'position' в консоль.
+
+
+// Ex.4 - let, const and var
+// 1. Запишите значение в 'projectName' в соответствии с названием проекта над которым работаете
+// 2. Подумайте, какие из созданных ранее переменных следует объявить через "const" (какие значения могут измениться, а какие нет?). Затем измените эти переменные, используя "const".
+
+
+
+// Ex5. - найди ошибку (английский)
+// This code should store "jsfor.qa" as a variable called name but it's not working. Can you figure out why?
+
+// const a == "jsfo";
+// const b == "r.qa";
+// const name == a + b;
+
+
+// one more time, expected result when console.log(name) should be "jsfor.qa"
+
+
+// ЗАДАЧИ СО ЗВЕЗДОЧКОЙ =) 
+// Для тех кто хочет попрактиковаться в знаниях, которые, возможно, имеет и решить простые задачи из тем, которые будут далее и познакомиться с нативными объектами и функциями (мы их немного рассмотрели в типах уже):
+// Ex6. - написать функцию, которая будет суммировать строки математичесски. Пример:
+// "4", "5" --> "9"
+//  "34", "5" --> "39" 
+// "", "" --> "0" 
+// "2", "" --> "2"
+//  "-5", "3" --> "-2"
+
+
+
+// function sumStr(a,b) {
+  
+// }
+
+
+// Ex7. - написать функцию, которая принимает два аргумента и возвращает (логает) остачу после деления БОЛЬШЕГО аргумента на МЕНЬШЕЕ (не просто первое на второе)
+// remainder(17, 5) -> 2
+// remainder(13, 72) -> remainder(72,13) -> 7 (The larger number should divide the smaller number)
+// remainder(1, 0) -> NaN (Division by 0)
+// remainder(0, 0) -> NaN (Division by 0)
+
+// function remainder(a, b){
+  // Divide the larger argument by the smaller argument and return the remainder
+  
+// }
+
+
+// Ex8. - Get opposite number
+// Very simple, given a number, find its opposite.
+// Examples:
+// 1: -1 
+// 14: -14 
+// -34: 34  
+
+// function opposite(number) {
+  //your code
+// }
+
+
+// Ex9. - Number to string
+// We need a function that can transform a number into a string.
+// What ways of achieving this do you know?Examples:
+// 123 --> "123"
+//  999 --> "999"
+
+
+
+// function numberToString(num) {
+  // Return a string of the number here!
+// }
